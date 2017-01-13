0329d0e439969c831a094dacd3f8bcf41df7348a
Oz <o.lodriguez@gmail.com>
2017-01-13 09:10:31 +0100

    Created singleton out of urban airship

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -6,6 +6,10 @@ application.on(application.launchEvent, function (args) {
 		developmentAppKey: "G0mIxntETJKumFTCWGfaNQ",
 		developmentAppSecret: "p0EMVN6zSmqSf5uxa1oP-w",
 		gcmSender: "758734661063",
+		detectProvisioningMode: false,
+		inProduction: false,
+		productionAppKey: "production key here",
+		productionAppSecret: "production secret here",
 	});
 });
 
--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,22 +1,20 @@
-import {Observable} from 'data/observable';
-import {NsUrbanairship} from 'nativescript-ns-urbanairship';
+import { Observable } from 'data/observable';
+import { NsUrbanairship } from 'nativescript-ns-urbanairship';
 
 export class HelloWorldModel extends Observable {
   public message: string;
-  private nsUrbanairship: NsUrbanairship;
 
   constructor() {
     super();
 
-    this.nsUrbanairship = new NsUrbanairship();
     this.message = 'ED Push test';
   }
 
   public registerClicked() {
-    this.nsUrbanairship.enablePush("MyTestNameUserID3");
+    NsUrbanairship.getInstance().enablePush("MyTestNameUserID3");
   }
 
   public unregisterClicked() {
-    this.nsUrbanairship.disablePush();
+    NsUrbanairship.getInstance().disablePush();
   }
 }
