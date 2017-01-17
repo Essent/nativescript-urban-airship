94aaf459616c35aa5a48cf7b8cf2b2beae810bec
Oz <o.lodriguez@gmail.com>
2017-01-17 16:22:01 +0100

    removed demo keys

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -3,10 +3,6 @@ import { NsUrbanairship } from 'nativescript-ns-urbanairship';
 
 application.on(application.launchEvent, function (args) {
 	let nsUrbanairship = new NsUrbanairship();
-	nsUrbanairship.developmentAppKey = "G0mIxntETJKumFTCWGfaNQ";
-	nsUrbanairship.developmentAppSecret = "p0EMVN6zSmqSf5uxa1oP-w";
-	nsUrbanairship.gcmSender = "758734661063";
-	nsUrbanairship.startUp();
 });
 
 application.start({ moduleName: "main-page" });
