3f3088014ae6964b81a24de616310ae66d904049
Oz <o.lodriguez@gmail.com>
2017-02-14 17:46:45 +0100

    updated comment for extended application

--- demo/app/application.android.ts
+++ demo/app/application.android.ts
@@ -2,6 +2,13 @@ import * as application from 'application';
 
 declare const com: any;
 
+/**
+ * Alter your Application name with caution, this requires you to change your application name inside of your App_Resources > AndroidManifest.xml file
+ * Even though, you do need to extend this class to make the takeoff call possible
+ * Keep in mind that anything referencing the previous class: com.tns.NativeScriptApplication will no longer work for Android.
+ * If you are having issues because you or a 3rd party plugin is referencing the old class directly. 
+ * In the plugin or code that refer to com.tns.NativeScriptApplication directly, Consider using: application.android.context instead.
+ **/ 
 @JavaProxy('com.tns.YourApplicationName')
 class YourApplicationName extends android.app.Application {
     public onCreate(): void {
