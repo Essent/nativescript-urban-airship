e4d6cb328a07ea4d56710430c06ca99bd4a7d8c5
Dennis van den Berg <dennis.vanden.berg@essent.nl>
2019-01-09 14:26:34 +0100

    Completed hook for firebase adjustments

--- demo/hooks/after-prepare/firebase-adjust-gradle.js
+++ demo/hooks/after-prepare/firebase-adjust-gradle.js
@@ -4,30 +4,52 @@ var fs = require('mz/fs');
 module.exports = function(logger, projectData, hookArgs) {
 
     return new Promise(function(resolve, reject) {
-        logger.info('====== starting firebase asjust gradle');
-
         if (hookArgs.platform.toLowerCase() === 'android') {
-            var destinationGradle = path.join(projectData.platformsDir, "android", "app", "build.gradle");
 
-            fs.exists(destinationGradle).then(function (exists) {
+            // Change build.gradle in app folder
+            var appGradle = path.join(projectData.platformsDir, "android", "app", "build.gradle");
+
+            fs.exists(appGradle).then(function (exists) {
                 if (!exists) {
                     reject('Missing app gradle file');
                 }
             });
 
-            fs.readFile(destinationGradle)
+            fs.readFile(appGradle)
                 .then(function (data) {
                     return data.toString();
                 })
                 .then(function (fileContent) {
                     if (!fileContent.toString().match(/^\s*apply plugin: 'com.google.gms.google-services'/mg)) {
-                        var newFileContent = fileContent + '\napply plugin: \'com.google.gms.google-services\'';
                         logger.info('Add "apply plugin: \'com.google.gms.google-services\'"');
-                        return fs.appendFile(destinationGradle, newFileContent);
+                        return fs.appendFile(appGradle, '\napply plugin: \'com.google.gms.google-services\'');
                     }
                     logger.warn('"apply plugin: \'com.google.gms.google-services\'" already added');
-                    // resolve();
+                });
+
+            // Change build.gradle in root folder
+            var mainGradle = path.join(projectData.platformsDir, "android", "build.gradle");
+
+            fs.exists(mainGradle).then(function (exists) {
+                if (!exists) {
+                    reject('Missing main gradle file');
+                }
+            });
+
+            fs.readFile(mainGradle)
+                .then(function (data) {
+                    return data.toString();
+                })
+                .then(function (fileContent) {
+                    if (!fileContent.toString().match(/^\s*classpath 'com.google.gms:google-services:4.2.0'/mg)) {
+                        var newFileContent = fileContent.replace("dependencies {", "dependencies {\nclasspath 'com.google.gms:google-services:4.2.0'");
+                        logger.info('Add "classpath \'com.google.gms:google-services:4.2.0\'"');
+                        return fs.writeFile(mainGradle, newFileContent);
+                    }
+                    logger.warn('"classpath \'com.google.gms:google-services:4.2.0\'" already added');
                 });
         }
+
+        resolve();
     });
 };
